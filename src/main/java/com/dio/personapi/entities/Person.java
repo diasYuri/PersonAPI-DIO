bbf29ef1154923bb1ad058fde413e1dfbce4fc9b
Yuri Dias <yuri.dazevedo@gmail.com>
2021-05-18 10:56:51 -0300

    Create person and phone entities

---
+++ src/main/java/com/dio/personapi/entities/Person.java
@@ -0,0 +1,17 @@
+package com.dio.personapi.entities;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate birthDate;
+
+
+
+}
---
+++ src/main/java/com/dio/personapi/entities/Phone.java
@@ -0,0 +1,13 @@
+package com.dio.personapi.entities;
+
+import com.dio.personapi.enums.PhoneType;
+
+public class Phone {
+
+    private Long id;
+
+    private String number;
+
+    private PhoneType type;
+
+}
