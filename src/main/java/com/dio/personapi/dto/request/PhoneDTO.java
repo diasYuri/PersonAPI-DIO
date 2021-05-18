182540b54d1fd91bbd61e1e1b0ea954146476379
Yuri Dias <yuri.dazevedo@gmail.com>
2021-05-18 12:26:11 -0300

    feat: date validation on PhoneDTO

--- src/main/java/com/dio/personapi/dto/request/PhoneDTO.java
+++ src/main/java/com/dio/personapi/dto/request/PhoneDTO.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 @Data
@@ -16,9 +18,12 @@ public class PhoneDTO {
 
     private Long id;
 
+    @NotEmpty
     @Enumerated(EnumType.STRING)
     private PhoneType type;
 
+    @NotEmpty
+    @Size(min = 13, max = 14)
     private String number;
 
 }
